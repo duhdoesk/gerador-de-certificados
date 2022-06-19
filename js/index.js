c313bb41ded75e42985f1f035c49fb601b76ae0a
duhdoesk <dev.edusc@gmail.com>
2022-06-19 20:42:31 -0300

    implemented CPF validation

--- js/index.js
+++ js/index.js
@@ -1,16 +1,41 @@
-function funcao() {
-    const nameValue = document.getElementById("name").value;
+const campoCPF = document.querySelector('#cpf');
+const botaoEmitir = document.querySelector('#emitir');
 
+function funcao() {
+    const cpfValue = document.getElementById("cpf").value;
+    
+    readTextFile("data/data.json", function(text){
+        let dados = JSON.parse(text);
+        let match = false;
+        
+        for (i of dados) {
+            if (i.cpf == cpfValue) {
+                gerarPDF(i.nome);
+                match = true;
+                break;
+            }
+        };
+
+        if (!match) {
+            alert('Desculpe, CPF não localizado no banco de dados do curso.');
+        };
+
+        campoCPF.value = '';
+        campoCPF.focus();
+    })
+}
+
+function gerarPDF(nome) {
     let doc = new jsPDF({
         orientation: "landscape"
-    })
+    });
     
-    doc.addImage(imgData, 'PNG', 0, 0, 296, 209)
+    doc.addImage(imgData, 'PNG', 0, 0, 296, 209);
 
-    doc.setFont("times")
-    doc.setFontType('italic')
-    doc.setFontSize(48)
+    doc.setFont("times");
+    doc.setFontType('italic');
+    doc.setFontSize(48);
 
-    doc.text(nameValue, 25, 93)
-    doc.save("certificado.pdf")
+    doc.text(nome, 25, 93);
+    doc.save("certificado.pdf");
 }
